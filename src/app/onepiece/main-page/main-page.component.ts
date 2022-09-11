56fdade5aaa97c87d6ea9c301262f2a8daf7f477
Eduardo Reyes <eduardrc5@hotmail.com>
2022-09-11 00:43:57 -0500

    Interacción de personajes para listar y agregar por formulario

--- src/app/onepiece/main-page/main-page.component.ts
+++ src/app/onepiece/main-page/main-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IPersonaje {
+  nombre: string;
+  recompensa: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -7,8 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MainPageComponent implements OnInit {
+
+  personajes: IPersonaje[] = [
+    {
+      nombre: 'Luffy',
+      recompensa: 3000000000
+    },
+    {
+      nombre: 'Zoro',
+      recompensa: 1111000000
+    },
+    {
+      nombre: 'Sanji',
+      recompensa: 1035000000
+    }
+  ]
+
+  nuevoPersonaje: IPersonaje = {
+    nombre: '',
+    recompensa: 0
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  agregar = () => {
+
+    if (!!!this.nuevoPersonaje.nombre || this.nuevoPersonaje.recompensa <= 0) {
+      return;
+    }
+
+    this.personajes = [... this.personajes, this.nuevoPersonaje];
+    this.nuevoPersonaje = { nombre: '', recompensa: 0 };
   }
 }
